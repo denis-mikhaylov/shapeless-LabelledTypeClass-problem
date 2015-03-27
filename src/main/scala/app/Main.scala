edf8410665cef52588f50bf5481d21c59bd2b1ee
Denis Mikhailov <mda.mymail@gmail.com>
2015-03-27 17:11:48 +0300

    Remove  Def

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -3,12 +3,12 @@ package app
 import app.ast.Def
 import play.api.libs.json.{Writes, Json}
 
-class Main extends App {
+object   Main extends App {
   import generic.CoproductContainer.implicits._
   import generic.GenericWrites._
   import Writes._ // Used to introduce predefined Writes instances into implicit scope
 
-  val definition: Def = Form()
+  val definition = Form()
 
   val result = Json.toJson(definition)
 
