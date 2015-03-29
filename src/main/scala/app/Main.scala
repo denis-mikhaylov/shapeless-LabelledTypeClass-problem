54a855285c9a1766cf286f7b5fbef70095ac9bdd
Denis Mikhailov <mda.mymail@gmail.com>
2015-03-29 12:25:33 +0300

    Fix comments

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,13 +1,12 @@
 package app
 
 // To successfully compile
-
-import app.test.A // Comment this
+import app.test.A // 1. Comment this line out
 import shapeless.LabelledGeneric
 
 object Main extends App {
 
-//  Uncomment this
+//  2. Uncomment 3 lines below
 //  sealed trait A[+T]
 //
 //  case class A1[+T](t: T) extends A[T]
